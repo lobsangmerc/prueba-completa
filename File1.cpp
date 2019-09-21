9702800b2fa64b7ca62f2fb352fd44e5fcd90214
lobsangmerc <54291375+lobsangmerc@users.noreply.github.com>
2019-09-20 21:48:31 -0400

    inicio

---
+++ File1.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+
+
+  int valor1   ;
+int  main ()
+{
+	cout<<"Hola clase de algoritmo \n";
+
+
+
+	system ("pause")  ;
+	return 0;
+}
+
+
+
+
+
